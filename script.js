5c6a7df2a383b2312a8c4a61840977f274f58e9d
nikeedev <myftr12394@gmail.com>
2026-08-30 20:28:12 +0200

    up

--- script.js
+++ script.js
@@ -4,12 +4,31 @@ let src = (time >= 07 && time <= 18) ? "vita1.mp3" : "vita2.mp3";
 
 audio.src = src;
 
+let search_engine = "ddg";
+
 window.onload = () => {
     audio.play();
     search_input.focus();
 }
 
+const changeSE = (SE) => {
+    document.getElementById(search_engine).style.text_decoration = "none";
+    search_engine = SE;
+    document.getElementById(SE).style.text_decoration = "underline";
+}
+
 search.onsubmit = () => {
     event.preventDefault();
-    window.location.href = "https://duckduckgo.com/?q=" + encodeURIComponent(search_input.value)
+
+    switch (search_engine) {
+        case "ddg":
+            window.location.href = "https://duckduckgo.com/?q=" + encodeURIComponent(search_input.value)
+            break;
+        case "goog":
+            window.location.href = "https://google.com/?q=" + encodeURIComponent(search_input.value)
+            break;
+        case "searxng":
+            window.location.href = "https://s.stio.studio/search?q=" + encodeURIComponent(search_input.value)
+            break;
+    }
 }
