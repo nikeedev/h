67bdbc6c5348f45308d1e9447ec5cda8c2bf32a9
nikeedev <myftr12394@gmail.com>
2026-08-30 20:21:08 +0200

    up

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const time = new Date().getHours()
+
+let src = (time >= 07 && time <= 18) ? "vita1.mp3" : "vita2.mp3";
+
+audio.src = src;
+
 window.onload = () => {
     audio.play();
     search_input.focus();
